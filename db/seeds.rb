9caae41c93b0afba3b55f344ef0cf604955e440e
Usama Yousaf <usamayousaf177@gmail.com>
2024-03-25 18:19:38 -0700

    Added seed file and faker gem for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+5.times do |i|
+  Product.create!(
+    name: "Product #{i+1}",
+    price: rand(10.0..100.0).round(2),
+    status: 'approved',
+    created_at: Faker::Time.backward(days: 30)
+  )
+end
+
+Product.all.each do |product|
+  ApprovalQueue.create!(
+    product: product
+  )
+end
