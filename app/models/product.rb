84cd29d23abe919b7af35f67f44ba51170e6c06b
Usama Yousaf <usamayousaf177@gmail.com>
2024-03-25 19:50:09 -0700

    Updated price_range scope in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,8 +8,14 @@ class Product < ApplicationRecord
   scope :active, -> { where(status: 'approved') }
   scope :name_like, -> (name) { where("name LIKE ?", "%#{name}%") if name.present? }
   scope :price_range, -> (min_price, max_price) {
-    if min_price.present? || max_price.present?
-      where(price: (min_price || 0)..(max_price || Float::INFINITY))
+    if min_price.present? && max_price.present?
+      where(price: min_price..max_price)
+    elsif min_price.present?
+      where('price >= ?', min_price)
+    elsif max_price.present?
+      where('price <= ?', max_price)
+    else
+      all
     end
   }
   scope :posted_between, -> (min_posted_date, max_posted_date) {
